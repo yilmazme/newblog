2ff2264025143391d0f61bcaa5457ba95933afae
myilmaz <myilmaz@subilgi.com.tr>
2021-08-31 15:26:12 +0300

    first commit

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import sanityClient  from "@sanity/client";
+
+export default sanityClient({
+    projectId:"t8moewut",
+    dataset:"production"
+})
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "../css/Navbar.module.css"
+
+export default function Navbar() {
+    return (
+        <div className={style.navbar}>
+          <h1 className={style.h1}>Hola</h1>
+          <h1 className={style.h1}>Hola</h1>
+          <h1 className={style.h1}>Hola</h1>
+        </div>
+    )
+}

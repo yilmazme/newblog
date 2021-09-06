bd629d0969103cc79bc9aa4d2dee5924ded5900c
myilmaz <myilmaz@subilgi.com.tr>
2021-09-06 16:17:17 +0300

    sanity cdn allowed

--- src/client.js
+++ src/client.js
@@ -2,5 +2,7 @@ import sanityClient  from "@sanity/client";
 
 export default sanityClient({
     projectId:"t8moewut",
-    dataset:"production"
+    dataset:"production",
+    apiVersion: '2021-08-31', // 
+    useCdn: true, // `false` if you want to ensure fresh data
 })
